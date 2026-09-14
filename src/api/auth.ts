a7e63a000107b5940737dfc3529dc846feee48c9
Shokhrukh Ochilov <ochilovshokhrukh17@gmail.com>
2026-09-14 15:52:39 +0900

    fix: restore working unit tests

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,7 +1,7 @@
 import { IncomingHttpHeaders } from "http";
 
 export function getAPIKey(headers: IncomingHttpHeaders): string | null {
-  const authHeaderrr = headers["authorization"];
+  const authHeader = headers["authorization"];
   if (!authHeader) {
     return null;
   }
